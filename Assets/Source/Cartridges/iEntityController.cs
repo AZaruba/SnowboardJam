74ec43ef3c903f87e43226bb52613d955d377184
Andrew Zaruba <A.B.Zaruba@gmail.com>
2019-10-04 21:30:49 -0500

    refactored code and created interface for EntityControllers

---
+++ Assets/Source/Cartridges/iEntityController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface iEntityController{
+
+    void EnginePull();
+    void EngineUpdate();
+    void UpdateStateMachine();
+}
