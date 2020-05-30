f2a1f2aabcb8841e90790f71663f6a0da3b3d458
Andrew Zaruba <a.b.zaruba@gmail.com>
2020-05-30 17:24:09 -0500

    Updates temp

--- Assets/Source/StateMachine/States/PlayerCharacter/AirStates/AerialState.cs
+++ Assets/Source/StateMachine/States/PlayerCharacter/AirStates/AerialState.cs
@@ -44,37 +44,6 @@ public class AerialState : iState {
         c_aerialMoveData.f_verticalVelocity = vertVelocity;
     }
 
-    // TODO: add horizontal movement that takes minimal external input here
-    public void ActOld()
-    {
-        float airVelocity = c_playerData.f_currentAirVelocity;
-        float gravity = c_playerData.f_gravity;
-        float terminalVelocity = c_playerData.f_terminalVelocity;
-        float currentSpeed = c_playerData.f_currentSpeed;
-
-        Vector3 currentDir = c_playerData.v_currentDirection;
-        Vector3 position = c_playerData.v_currentPosition;
-        Vector3 oldPosition = position;
-
-        cart_gravity.UpdateAirVelocity(ref airVelocity, gravity, terminalVelocity);
-        cart_velocity.UpdatePosition(ref position, ref currentDir, ref currentSpeed);
-        position.y += airVelocity * Time.deltaTime;
-
-        c_playerData.v_currentPosition = position;
-        c_playerData.f_currentAirVelocity = airVelocity;
-        c_playerData.v_currentAirDirection = Vector3.Normalize(position - oldPosition);
-        c_playerData.v_currentDown = c_playerData.v_currentAirDirection;
-        if (airVelocity <= Constants.ZERO_F)
-        {
-            c_playerData.f_currentRaycastDistance = c_playerData.f_raycastDistance + Mathf.Abs(airVelocity) * Time.deltaTime;
-        }
-        else
-        {
-            c_playerData.f_currentRaycastDistance = Constants.ZERO_F;
-        }
-    }
-    
-    // TODO: Fix the lateral movement preservation (if we're flat, why did it get reduced once we fall?)
     public void TransitionAct()
     {
 
@@ -95,32 +64,24 @@ public class AerialState : iState {
         c_aerialMoveData.v_lateralDirection = latDir;
     }
 
-    public void TransitionActOld()
-    {
-        Vector3 previousDirection = c_playerData.v_currentDirection;
-        float currentVelocity = c_playerData.f_currentSpeed;
-        float airVelocity = previousDirection.y * currentVelocity;
-        Vector3 airDirection = previousDirection;
-        airDirection.Normalize();
-
-        previousDirection.y = Constants.ZERO_F; // "flatten direction"
-
-        // scale velocity by the change in magnitude so we don't go faster in a direction
-        // float magnitudeFactor = previousDirection.magnitude / c_playerData.v_currentDirection.magnitude;
-
-        c_playerData.f_currentAirVelocity = airVelocity;
-        c_playerData.v_currentDirection = previousDirection.normalized;
-        c_playerData.v_currentAirDirection = airDirection;
-        c_playerData.f_currentSpeed *= previousDirection.magnitude;
-        c_playerData.v_currentDown = Vector3.down;
-        c_playerData.f_currentJumpCharge = Constants.ZERO_F;
-    }
-
     // TODO: Fix behavior when we he the ground, we're back to just bumping back up
     public StateRef GetNextState(Command cmd)
     {
         if (cmd == Command.LAND)
         {
+            /*
+            // take normalize vertDir and latDir, that is our new direction.
+            Vector3 newDirection = c_aerialMoveData.v_lateralDirection.normalized * c_aerialMoveData.f_lateralVelocity;
+            newDirection.y = c_aerialMoveData.f_lateralVelocity;
+
+            if (!float.Equals(newDirection.magnitude, Constants.ZERO_F))
+            {
+                c_playerData.v_currentDirection = newDirection.normalized;
+            }
+
+            c_playerData.f_currentSpeed = newDirection.magnitude;
+            */
+
             return StateRef.GROUNDED;
         }
         if (cmd == Command.CRASH)
