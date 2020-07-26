28ee699bce379714a7a105c08054b25a93498b8a
Andrew Zaruba <a.b.zaruba@gmail.com>
2020-07-26 14:36:52 -0500

    Stat Display Update Finished!
    Placeholder art is still in place, but the basic effect of stat display updating depending on selected character is complete. There are little updates that can be done here and there, but it's mostly visual design. The next big programming task for this is the shaders.

--- Assets/Source/Cartridges/MenuCartridges/NonItem/CharacterStatDisplayController.cs
+++ Assets/Source/Cartridges/MenuCartridges/NonItem/CharacterStatDisplayController.cs
@@ -21,6 +21,10 @@ public class CharacterStatDisplayController : MonoBehaviour
     public int currentIndex;
     private int currentActiveIndex;
 
+    private float targetSpeedAlpha;
+    private float targetBalanceAlpha;
+    private float targetTrickAlpha;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -41,13 +45,9 @@ public class CharacterStatDisplayController : MonoBehaviour
             float currentBalance = CharacterAttributes[currentActiveIndex].Balance;
             float currentTricks = CharacterAttributes[currentActiveIndex].Tricks;
 
-            float currentSpeedAlpha = Mathf.Lerp(0.95f, 1.0f, currentSpeed / currentAlphaMax); //1 - (currentSpeed / currentAlphaMax); 
-            float currentBalanceAlpha = Mathf.Lerp(0.95f, 1.0f, currentBalance / currentAlphaMax);
-            float currentTricksAlpha = Mathf.Lerp(0.95f, 1.0f, currentTricks / currentAlphaMax);
-
-            SelectedCharacterSpeedDisplay.material.SetFloat("_Cutoff", currentSpeedAlpha);
-            SelectedCharacterBalanceDisplay.material.SetFloat("_Cutoff", currentBalanceAlpha);
-            SelectedCharacterTricksDisplay.material.SetFloat("_Cutoff", currentTricksAlpha);
+            targetSpeedAlpha = Mathf.Lerp(1.0f, 0, currentSpeed / currentAlphaMax); //1 - (currentSpeed / currentAlphaMax); 
+            targetBalanceAlpha = Mathf.Lerp(1.0f, 0, currentBalance / currentAlphaMax);
+            targetTrickAlpha = Mathf.Lerp(1.0f, 0, currentTricks / currentAlphaMax);
 
             // debug values
             SelectedCharacterNameText.text = CharacterAttributes[currentActiveIndex].Name;
@@ -55,5 +55,21 @@ public class CharacterStatDisplayController : MonoBehaviour
             SelectedCharacterBalanceText.text = "BALANCE: " + CharacterAttributes[currentActiveIndex].Balance;
             SelectedCharacterTricksText.text = "TRICKS: " + CharacterAttributes[currentActiveIndex].Tricks;
         }
+
+        // it's easier to just do a bespoke implementation rather than force the state machine pattern here
+        float currentSpeedAlpha = SelectedCharacterSpeedDisplay.material.GetFloat("_Cutoff");
+        float currentBalanceAlpha = SelectedCharacterBalanceDisplay.material.GetFloat("_Cutoff");
+        float currentTricksAlpha = SelectedCharacterTricksDisplay.material.GetFloat("_Cutoff");
+
+        float target = Mathf.Lerp(currentSpeedAlpha, targetSpeedAlpha, 0.01f);
+        SelectedCharacterSpeedDisplay.material.SetFloat("_Cutoff", target);
+
+        target = Mathf.Lerp(currentBalanceAlpha, targetBalanceAlpha, 0.01f);
+        SelectedCharacterBalanceDisplay.material.SetFloat("_Cutoff", target);
+
+        target = Mathf.Lerp(currentTricksAlpha, targetTrickAlpha, 0.01f);
+        SelectedCharacterTricksDisplay.material.SetFloat("_Cutoff", target);
+
+
     }
 }
