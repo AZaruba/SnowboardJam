a442b7ec741453a2fdcf2de3e9308f91474d5fb2
Andrew Zaruba <a.b.zaruba@gmail.com>
2021-12-30 20:02:24 -0600

    No Alloc timer Update
    The timer now works and does not allocate any memory. The only source of allocations is the debug output which can easily be removed in a future update. According to the profiler, this means only infrequent allocations to handle later

--- Assets/Source/Controllers/UIControllers/TimerController2.cs
+++ Assets/Source/Controllers/UIControllers/TimerController2.cs
@@ -8,7 +8,11 @@ public class TimerController2 : MonoBehaviour
     [SerializeField] private Text displaySeconds;
     [SerializeField] private Text displayMinutes;
     [SerializeField] private Text displayMilliSeconds;
-    private float time = 60000;
+    private float time = 0;
+
+    private StateMachine sm_timer;
+    private iMessageClient cl_timer;
+    private StateData c_stateData;
 
     CacheIntString cacheSeconds = new CacheIntString(
         (seconds) => seconds % 60, //describe how seconds (key) will be translated to useful value (hash)
@@ -30,17 +34,44 @@ public class TimerController2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        c_stateData = new StateData();
+        c_stateData.b_updateState = true;
+
+        TimerActiveState s_active = new TimerActiveState();
+
+        sm_timer = new StateMachine(StateRef.TIMER_STEP);
+        sm_timer.AddState(s_active, StateRef.TIMER_STEP);
+
+        InitializeMessageClient();
     }
 
     // Use update instead of fixed update as we don't really "care" about simulation here
     void Update()
     {
-        this.time += Time.deltaTime;
+        if (!c_stateData.b_updateState)
+        {
+            return;
+        }
+
+        if (c_stateData.b_preStarted)
+        {
+            return;
+        }
+
+
+        time += Time.deltaTime;
         int seconds = Mathf.FloorToInt(time);
 
         displaySeconds.text = cacheSeconds[seconds];
         displayMinutes.text = cacheMinutes[seconds];
         displayMilliSeconds.text = cacheMilliSeconds[time];
+    }
+
+    private void InitializeMessageClient()
+    {
+        cl_timer = new TimerMessageClient(ref c_stateData);
+        MessageServer.Subscribe(ref cl_timer, MessageID.PAUSE);
+        MessageServer.Subscribe(ref cl_timer, MessageID.PLAYER_FINISHED);
+        MessageServer.Subscribe(ref cl_timer, MessageID.COUNTDOWN_OVER);
     }
 }
---
+++ Assets/Source/StateMachine/States/Timer/TimerActiveState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerActiveState : iState
+{
+    // no state actions, just here to interact with pre-start and pausing
+    public TimerActiveState()
+    {
+
+    }
+
+    public void Act()
+    {
+    }
+
+    public StateRef GetNextState(Command cmd)
+    {
+        return StateRef.TIMER_STEP;
+    }
+
+    public void TransitionAct()
+    {
+
+    }
+}
